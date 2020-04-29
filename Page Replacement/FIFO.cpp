241f26eb22cdd79610e8c027913966a483bc6ffb
Prince Bansal <princebansal42@gmail.com>
2020-04-29 20:28:22 +0530

    slight edit

--- Page Replacement/FIFO.cpp
+++ Page Replacement/FIFO.cpp
@@ -1,49 +1,48 @@
-#include<bits/stdc++.h> 
-using namespace std; 
-int pageFaults(int pages[], int n, int capacity) 
-{ 
-	set<int> s; 
-	queue<int> indexes; 
-	int page_faults = 0; 
-	for (int i=0; i<n; i++) 
-	{ 
-		if (s.size() < capacity) 
-		{ 
-			if (s.find(pages[i])==s.end()) 
-			{ 
-				s.insert(pages[i]); 
-				page_faults++; 
-				indexes.push(pages[i]); 
-			} 
-		} 
+#include <bits/stdc++.h>
+using namespace std;
+int pageFaults(int pages[], int n, int capacity)
+{
+	set<int> s;
+	queue<int> indexes;
+	int page_faults = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (s.size() < capacity)
+		{
+			if (s.find(pages[i]) == s.end())
+			{
+				s.insert(pages[i]);
+				page_faults++;
+				indexes.push(pages[i]);
+			}
+		}
 		else
-		{ 
-			if (s.find(pages[i]) == s.end()) 
-			{ 
-				int val = indexes.front(); 
-				indexes.pop(); 
-				s.erase(val); 
-				s.insert(pages[i]); 
-				indexes.push(pages[i]); 
-				page_faults++; 
-			} 
-		} 
-	} 
+		{
+			if (s.find(pages[i]) == s.end())
+			{
+				int val = indexes.front();
+				indexes.pop();
+				s.erase(val);
+				s.insert(pages[i]);
+				indexes.push(pages[i]);
+				page_faults++;
+			}
+		}
+	}
 
-	return page_faults; 
-} 
-int main() 
-{ 
+	return page_faults;
+}
+int main()
+{
 	int n;
-	cout<<"Enter the total number of pages: ";
-	cin>>n;
+	cout << "Enter the total number of pages: ";
+	cin >> n;
 	int pages[n], capacity;
-	cout<<"Enter the capacity of queue: ";
-	cin>>capacity;
-	cout<<"Enter the sequence of pages:-\n";
-	for(int i=0;i<n;i++) 
-		cin>>pages[i];
-	cout <<"Number of hits were: "<<n-pageFaults(pages, n, capacity); 
-	return 0; 
-} 
-
+	cout << "Enter the capacity of queue: ";
+	cin >> capacity;
+	cout << "Enter the sequence of pages:-\n";
+	for (int i = 0; i < n; i++)
+		cin >> pages[i];
+	cout << "Number of hits were: " << n - pageFaults(pages, n, capacity);
+	return 0;
+}
--- Thread.cpp
+++ Thread.cpp
@@ -4,25 +4,29 @@
 
 using namespace std;
 
-void *PrintHello(void *threadid) {
+void *PrintHello(void *threadid)
+{
    long tid;
    tid = (long)threadid;
-   cout << "Thread ID, " << tid <<" says Hello to the World"<< endl;
+   cout << "Thread ID, " << tid << " says Hello to the World" << endl;
    pthread_exit(NULL);
 }
 
-int main () {
-	int n;
-	cout<<"Enter the total number of threads to be created: ";
-	cin>>n;
-   	pthread_t threads[n];
-   	int rc;
-   	int i;
-    for( i = 0; i < n; i++ ) {
+int main()
+{
+   int n;
+   cout << "Enter the total number of threads to be created: ";
+   cin >> n;
+   pthread_t threads[n];
+   int rc;
+   int i;
+   for (i = 0; i < n; i++)
+   {
       cout << "main() : creating thread, " << i << endl;
       rc = pthread_create(&threads[i], NULL, PrintHello, (void *)i);
-      
-      if (rc) {
+
+      if (rc)
+      {
          cout << "Error:unable to create thread," << rc << endl;
          exit(-1);
       }
